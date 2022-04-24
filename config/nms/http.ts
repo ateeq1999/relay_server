d95c08ab843434283834c01a73fa9dade29db124
Ateeq <ateeg1999@gmail.com>
2022-04-24 20:17:09 +0200

    edit configs for ports

--- config/nms/http.ts
+++ config/nms/http.ts
@@ -2,7 +2,7 @@ import Env from '@ioc:Adonis/Core/Env'
 
 const httpConfig = {
     http: {
-        port: 5050,
+        port: Env.get('HTTP_PORT'),
         mediaroot: Env.get('NMS_HTTP_MEDIA_ROOT'),
         allow_origin: '*'
     }
