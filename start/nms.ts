4cf0ae81b3877533515696ac984a487375deaa8d
Ateeq <ateeg1999@gmail.com>
2022-05-09 12:01:55 +0200

    install new packege for get ffmpeg path

--- start/nms.ts
+++ start/nms.ts
@@ -1,5 +1,8 @@
 import nmsConfig from 'Config/nms';
 import NodeMediaServer from 'node-media-server'
+import FFmpegInstaller from '@ffmpeg-installer/ffmpeg';
+
+console.log("ffmpeg path",FFmpegInstaller.path)
 // import nmsHooks from './nmsHooks'
   
 var nms = new NodeMediaServer(nmsConfig)
