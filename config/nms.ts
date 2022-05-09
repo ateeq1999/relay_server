f6cea838534701b5f2a0e404cff00dc9c4ddf521
Ateeq <ateeg1999@gmail.com>
2022-05-09 12:07:01 +0200

    remove new packege code for get ffmpeg path

--- config/nms.ts
+++ config/nms.ts
@@ -1,12 +1,11 @@
-// import Env from '@ioc:Adonis/Core/Env'
+import Env from '@ioc:Adonis/Core/Env'
 import AuthConfig from './nms/auth'
 import FissionServer from './nms/fissionServer'
 import httpConfig from './nms/http'
 import RelayServer from './nms/relayServer'
 import rtpmConfig from './nms/rtmp'
 import TransServer from './nms/transServer'
-import FFmpegInstaller from '@ffmpeg-installer/ffmpeg';
-// const ffmpeg = require('@ffmpeg-installer/ffmpeg');
+// import FFmpegInstaller from '@ffmpeg-installer/ffmpeg';
 
 const nmsConfig = {
     logType: 3,
@@ -14,18 +13,18 @@ const nmsConfig = {
     auth: AuthConfig.auth,
     http: httpConfig.http,
     trans: {
-        ffmpeg: FFmpegInstaller.path,
-        // ffmpeg: Env.get('FFMPEG_BIN_PATH'),
+        // ffmpeg: FFmpegInstaller.path,
+        ffmpeg: Env.get('FFMPEG_BIN_PATH'),
         tasks: TransServer.tasks,
     },
     relay: {
-        ffmpeg: FFmpegInstaller.path,
-        // ffmpeg: Env.get('FFMPEG_BIN_PATH'),
+        // ffmpeg: FFmpegInstaller.path,
+        ffmpeg: Env.get('FFMPEG_BIN_PATH'),
         tasks: RelayServer.tasks
     },
     fission: {
-        ffmpeg: FFmpegInstaller.path,
-        // ffmpeg: Env.get('FFMPEG_BIN_PATH'),
+        // ffmpeg: FFmpegInstaller.path,
+        ffmpeg: Env.get('FFMPEG_BIN_PATH'),
         tasks: FissionServer.tasks
     }
 }
--- start/nms.ts
+++ start/nms.ts
@@ -1,8 +1,6 @@
 import nmsConfig from 'Config/nms';
 import NodeMediaServer from 'node-media-server'
-import FFmpegInstaller from '@ffmpeg-installer/ffmpeg';
 
-console.log("ffmpeg path",FFmpegInstaller.path)
 // import nmsHooks from './nmsHooks'
   
 var nms = new NodeMediaServer(nmsConfig)
